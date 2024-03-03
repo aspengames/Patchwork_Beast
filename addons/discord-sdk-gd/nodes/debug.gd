fe27f334d47a48ae4964e531200c7b712dc388ac
seanhlewis <96705270+seanhlewis@users.noreply.github.com>
2024-03-02 23:40:38 -0600

    deletion

--- addons/discord-sdk-gd/nodes/debug.gd
+++
@@ -1,7 +0,0 @@
-## This is a Debug Node wich will show (only if the project runs) some usefull info and buttons/input
-extends Node
-
-func _ready() -> void:
-	if globals.discord_enabled:
-		const DebugNodeGroup: PackedScene = preload("res://addons/discord-sdk-gd/nodes/Debug.tscn")
-		add_child(DebugNodeGroup.instantiate())
